728cc45c6a11bd5b390c51e33e61345d0f98158c
SATYAM <126665079+Raichuop07@users.noreply.github.com>
2023-12-19 10:04:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -38,7 +38,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Pranav-Saraswat/Testing-Alpha1",
+    "https://github.com/Jatindalal875/Doremoon21",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
